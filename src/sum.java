9a52679ee8f44f14723ad607b2674e77f24e40b1
doyeon kim <kkdy0416@naver.com>
2025-01-04 19:26:02 +0900

    third commit

---
+++ src/sum.java
@@ -0,0 +1,24 @@
+// LOY 스터디 - 1주차 문제 2번
+import java.util.Scanner;
+
+public class sum {
+     public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+
+    System.out.println("결과: ");
+
+    System.out.print("num1 : ");
+    int x = scanner.nextInt();
+
+    System.out.print("num2 : ");
+    int y = scanner.nextInt(); 
+    
+    int sum = 0 ;
+    
+    for ( int i = x; i<= y; i++){
+        sum += i;   
+    }
+    System.out.println(sum);
+}
+}
