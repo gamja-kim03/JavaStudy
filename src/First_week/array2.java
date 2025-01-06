f84eb28088015583feb6e5a98e7c760f719603fb
doyeon kim <kkdy0416@naver.com>
2025-01-06 21:54:37 +0900

    update

---
+++ src/First_week/array2.java
@@ -0,0 +1,20 @@
+// 5번 문제
+package First_week;
+public class array2 {
+    public static void main(String[] args) {
+        System.out.println("결과 :");
+    
+        int[] Original = new int[]{ 5,6,3,7,8,9 };
+        System.out.print("Original Array : ");
+            for(int i = 0; i < Original.length; i++) {
+            System.out.print(Original[i] + " , ");
+            }
+        System.out.println("");
+    
+        System.out.print("New Array : ");
+        for(int i = Original.length-1; i >=0; i--) {
+            System.out.print(Original[i] + " , ");
+            }
+    }
+    }
+    
