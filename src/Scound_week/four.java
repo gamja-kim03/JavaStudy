27f45d63ec9364b12f5d868e2316f57aab48e5f6
doyeon kim <kkdy0416@naver.com>
2025-01-09 23:26:11 +0900

    update

--- src/Scound_week/four.java
+++ src/Scound_week/four.java
@@ -9,11 +9,13 @@ public class four {
         System.out.print("input number : ");
         int x = scanner.nextInt();
 
-        for (int i = 1; i <= x; i++) {
-            for (int j = 0; j < x - i; j++) {
+        for (int i = 1; i <= x; i++) { //i를 1부터 x까지 반복복
+            for (int j = 0; j < x - i; j++) { 
+                //별을 오른쪽으로 정렬해야하니까 왼쪽부분은 공백으로 출력                
                 System.out.print(" ");
             }
             for (int j = 0; j < i; j++) {
+                // j를 사용하여 i만큼 별을 출력
                 System.out.print('*');
             }
             System.out.println();
