f50f17f796b0e6eba95ff8e5fcf6088d67854f1e
doyeon kim <kkdy0416@naver.com>
2025-01-11 17:55:57 +0900

    update

---
+++ src/Scound_week/count.java
@@ -0,0 +1,12 @@
+package Scound_week;
+
+public class count {
+
+    public static void main(String[] args) {
+
+        int[] array = new int[]{4, 3, 6, 7, 8, 3, 4, 5, 3, 4, 1};
+        int x = array.length;
+        System.out.println(x);
+    }
+
+}
