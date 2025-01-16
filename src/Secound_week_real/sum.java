3e6d69d239d4ddf66dbf80e14199e391b60f7545
doyeon kim <kkdy0416@naver.com>
2025-01-17 00:16:33 +0900

    update

---
+++ src/Secound_week_real/sum.java
@@ -0,0 +1,34 @@
+package Secound_week_real;
+
+public class sum {
+    public static void main(String[] args) {
+        System.out.println("결과");
+
+        System.out.print("arry 1 : ");
+
+        int[] arr1 = new int[]{2,4,5,6,3};
+
+        for (int i = 0; i < arr1.length; i++) {
+            System.out.print(arr1[i] + ",");
+        }
+        System.out.println("");
+
+        int[] arr2 = new int[]{1,3,4,5,6};
+
+        System.out.print("arry 2 : ");
+
+        for (int i = 0; i < arr2.length; i++) {
+            System.out.print(arr2[i] + ",");
+        }
+        
+        System.out.println("");
+
+        System.out.println("merge array : ");
+
+        int  [] arr3 = new int[]{};
+        
+       
+    }
+
+    
+}
