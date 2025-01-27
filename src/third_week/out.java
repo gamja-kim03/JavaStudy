55844c1051972ee23699c9f09b52734f36097391
doyeon kim <kkdy0416@naver.com>
2025-01-27 23:07:37 +0900

    update

--- src/third_week/out.java
+++ src/third_week/out.java
@@ -13,11 +13,20 @@ public class out {
         String y ="";
 
         for (int i = 0; i < x.length(); i++){
+            char c = x.charAt(i);
+            if (c != 'a' && c != 'e' && c != 'i' && c != 'o' && c != 'u' &&
+                c != 'A' && c != 'E' && c != 'I' && c != 'O' && c != 'U') {
+                y += c;
+            }
+        }
+
+        /* for (int i = 0; i < x.length(); i++){
             if (i != 'a' && i != 'e' && i != 'i' && i != 'o' && i != 'u' &&
                 i != 'A' && i != 'E' && i != 'I' && i != 'O' && i != 'U') {
                 y += i;
             }
-        }
+        } */
+       // 처음에 이렇게만 했는데 답이 숫자로 나오길래 수정했음
 
         System.out.println("모음이 제거된 문자열 : " + y);
     }
