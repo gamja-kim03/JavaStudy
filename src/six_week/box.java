824ae5eb87ef1f5d89f80c9126c46d2245fc3bb2
doyeon kim <kkdy0416@naver.com>
2025-02-08 23:07:30 +0900

    update

---
+++ src/six_week/box.java
@@ -0,0 +1,61 @@
+// 문제
+/* 
+ 다음 멤버를 가지고 직사각형을 표현하는 Rectangle 클래스를 작성하라.
+ - int 타입이 x,y width, height 필드 : 사각형을 구성하는 점과 크기 정보
+ - x,y width, height 값을 매개변수로 받아 필드를 초기화하는 생성자
+ - int square() : 사각형 넓이 리턴
+ - void show : 사격형의 좌표와 넓이를 화면에 출력
+ - boolean contains(Rectangle r): 매개변수로 받은 r이 현 사각형 안에 있으면 true 리턴
+ */
+
+package six_week;
+
+class Rectangle {
+    // 멤버변수 설정 및 초기화
+    int x = 0;
+    int y = 0;
+    int width = 0;
+    int height = 0;
+
+    public Rectangle(){
+    }
+
+    public Rectangle(int x, int y, int width, int height){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    // 사각형의 넓이를 반환하는 메서드
+    public int square(){
+        return width * height;
+    }
+
+    //사각형의 좌표 및 넓이를 출력하는 메서드
+    public void show(){
+        System.out.println("("+ x + "," + y + ")" + "에서 크기가 " + width+'x'+height+"인 사각형");
+    }
+
+    //매개변수로 받은 사각형 r이 현재 사각형 내부에 포함되는지 확인하는 메서드
+    /* public  boolean contains(){
+        return 
+    } */
+}
+public class box {
+    public static void main(String[] args) {
+        Rectangle r = new Rectangle(2,2,8,7);
+        Rectangle s = new Rectangle(5, 5,6,6);
+        Rectangle t = new Rectangle(1,1,10,10);
+
+        r.show();
+        System.out.println("s의 면적은 " + s.square());
+     /*   if(t.contains(r)){
+        System.out.println("t는 r을 포함합니다");
+       }
+      
+       if(t.contains(s)) {
+        System.out.println("t는 s를 포함합니다");
+       }    */
+    }   
+}
--- src/six_week/sumgame.java
+++ src/six_week/sumgame.java
@@ -1,8 +1,11 @@
+// 문제
+// 0~9 사이 임의 숫자 2개를 생성한 후 10개의 덧셈 문제 출제
+// 답을 입력 받고, 사용자가 맞힌 개수와 틀린 개수를 최종적으로 출력
+
 package six_week;
 
 import java.util.Random;
 import java.util.Scanner;
-
 
 public class sumgame {
     public static void main(String[] args) {
@@ -10,14 +13,37 @@ public class sumgame {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("다음 열 문제를 푸시오.");
-
-        while (true) { 
+        
+        int rightCount = 0; // 정답 카운트를 세야하니까 변수 생성 및 초기화
+        int wrongCount = 0; // 틀린 갯수도 세야하니까 틀렸을 때 변수 생성 및 초기화화
+        
+        // 문제를 10개 반복하기 위해 for문을 사용
+        for (int i = 0; i < 10; i++){
             int x = rand.nextInt(10);
             int y = rand.nextInt(10);
 
-            System.out.print(x + " + " + y+ " = ");
+            //System.out.println(i + "번째 문제 : " + x + '+' + y + '='); 
+            // i의 초기값을 0으로 했기 때문에 +1을 함
+
+            System.out.print((i+1) + "번째 문제 : " + x + '+' + y + '='); 
             int my= scanner.nextInt();
+
+            // if문을 사용해서 정답과 같으면 정답 카운트에 1을 추가하고 그렇지 않으면 틀린 카운트에 1을 추가함
+
+            if (answer(x, y, my)){
+                rightCount++;
+            }
+            else {
+                wrongCount++;
+            }
         }
 
+        System.out.println("맞힌 개수는 " + rightCount);
+        System.out.println("틀린 개수는 " + wrongCount);
+           
+    }
+    
+    public static boolean answer(int a, int b, int u){
+        return (a + b) == u;
     }
 }
