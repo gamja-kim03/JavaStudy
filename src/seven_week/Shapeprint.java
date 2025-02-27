952b80c37fd802eecdd1114ecfb103648e447891
doyeon kim <kkdy0416@naver.com>
2025-02-27 20:23:01 +0900

    update

--- src/seven_week/Shapeprint.java
+++ src/seven_week/Shapeprint.java
@@ -11,7 +11,7 @@ class Shape {
         System.out.println("Shape");
     }
 
-    // (3) s.print()가 호출될 때 "Shape"이 출력되도록 Shape에 print() 추가
+    // 3. s.print()가 호출될 때 "Shape"이 출력되도록 Shape에 print() 추가
     public void print() {
         System.out.println("Shape");
     }
@@ -19,7 +19,7 @@ class Shape {
 
 // 자식 클래스 Circle 정의 (Shape을 상속)
 class Circle extends Shape {
-    // (2) s.print()가 호출될 때 "Circle"이 출력되도록 구현
+    // 2. s.print()가 호출될 때 "Circle"이 출력되도록 구현
     @Override
     public void print(){
         System.out.println("Circle");
@@ -34,16 +34,16 @@ class Circle extends Shape {
 
 public class Shapeprint {
     public static void main(String[] args) {
-         // (1) 동적 바인딩 적용
+         // 1. 동적 바인딩 적용
         // 부모 타입(Shape) 변수 s가 자식 객체(Circle)를 참조
         Shape s = new Circle(); // 업캐스팅
         
         s.draw();// "Circle" 출력 (동적 바인딩으로 Circle의 draw()가 호출됨)
 
-        // (2) s.print()가 "Circle"을 출력하도록 구현됨
+        // 2. s.print()가 "Circle"을 출력하도록 구현됨
         s.print();  // "Circle" 출력 (Circle에서 print()를 오버라이딩했기 때문)
 
-        // (3) 부모 클래스의 print()가 실행되도록 함
+        // 3. 부모 클래스의 print()가 실행되도록 함
         Shape s1 = new Shape();
         s1.print(); // "Shape" 출력 (s1은 부모 객체이므로 부모의 print() 실행)
 
